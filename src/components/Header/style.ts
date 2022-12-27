498bea0a0c21ff6821f6ce6b0727e8de9a55487b
Lucas Samuel F. S <68969699+Dev3lopmentM4chine@users.noreply.github.com>
2022-12-27 18:09:33 -0300

    updat

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -4,18 +4,17 @@ export const StyledHeader = styled.header`
   width: 100%;
   height: 80px;
   background-color: var(--color-grey-0);
+  padding: 0px 10px 0px 10px;
+  
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: space-around;
   align-items: center;
 
   @media screen and (min-width: 800px) {
     flex-direction: row;
+    justify-content: space-between;
     padding: 0px 6px 0px 25px;
-  }
-
-  & > button[type="button"] {
-    background-color: gray;
   }
 
   & > .navbar{
@@ -23,5 +22,25 @@ export const StyledHeader = styled.header`
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
+  }
+
+  & > .navbar .searchDesktop{
+    form{
+      display: none;
+    }
+
+    button{
+      display: flex;
+    }
+
+    @media (min-width: 800px) {
+      form{
+        display: flex;
+      }
+
+      button[type='button']{
+        display: none;
+      }
+    }  
   }
 `;
